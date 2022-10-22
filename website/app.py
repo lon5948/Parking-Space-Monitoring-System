9476c6fbf9b3c7b4bba626d005233354bc36b35b
lon5948 <a0988282303@gmail.com>
2022-10-22 11:58:45 +0800

    change folder name

--- website/app.py
+++ website/app.py
@@ -1,11 +1,4 @@
-import os
-from flask_bcrypt import Bcrypt
 from flask import Flask, render_template, session, request, jsonify, flash, redirect, url_for
-from flask_paginate import Pagination
-from werkzeug.utils import secure_filename
-import mysql.connector
-import datetime
-import time
 
 app = Flask(__name__)
 
